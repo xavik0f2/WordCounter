2d0481488bd38fe1ae07c96456af135b57684e6d
Xavier Llanga <xavierllanga@hotmail.com>
2019-04-04 18:26:09 +0200

    initial commit

---
+++ WordCounterApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordCounterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
