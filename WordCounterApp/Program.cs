835cea4957e6f2372a898d80c23fa75f16be4948
Xavier Llanga <xavierllanga@hotmail.com>
2019-04-04 18:34:01 +0200

    Add flow application

--- WordCounterApp/Program.cs
+++ WordCounterApp/Program.cs
@@ -6,7 +6,11 @@ namespace WordCounterApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+              WordCounter text = new WordCounter("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500");
+
+            Console.WriteLine(text.TotalWords);
+
+            Console.ReadLine();
         }
     }
 }
