a6292846bb46e45a2d59d68bfd379dec4d1068a9
Xavier Llanga <xavierllanga@hotmail.com>
2019-04-04 18:36:13 +0200

    Implemented RemoveDuplicatedWords based on a method that Split words before

---
+++ WordCounterApp/WordCounter.cs
@@ -0,0 +1,49 @@
+using System;
+
+
+namespace WordCounter
+{
+    public class WordCounter
+    {
+        private string Text { set; get; }
+        private int TotalUniqueWords { get; set; }
+        public int TotalWords { get; }
+        public WordCounter(string text)
+        {
+            Text = text;
+            CountUniqueWords();
+            TotalWords = TotalUniqueWords;
+        }
+        private void CountUniqueWords()
+        {
+            if (ThereIsMoreThanOneWord())
+            {
+                TotalUniqueWords = RemoveDuplicatedWords().Count;
+                return;
+            }
+            if (!String.IsNullOrEmpty(Text))
+            {
+                TotalUniqueWords = 1;
+                return;
+            }
+            TotalUniqueWords = 0;
+        }
+
+        private Boolean ThereIsMoreThanOneWord()
+        {
+            if (Text.Contains(' '))
+                return true;
+
+            return false;
+        }
+
+        private List<string> RemoveDuplicatedWords()
+        {
+            return SplittedWords().Distinct().ToList();
+        }
+         private List<string> SplittedWords()
+        {
+            return new List<string>(Text.Split(' '));
+        }
+    }
+}
