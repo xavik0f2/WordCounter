aac5c32be9d182d94e4101c48b3d13f95e60ebcc
Xavier Llanga <xavier.llanga.perez@avanade.com>
2019-04-05 23:44:07 +0200

    Samples of each user story

--- WordCounterApp/Program.cs
+++ WordCounterApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Xml.Linq;
+using System.Linq;
+using System;
 
 namespace WordCounter
 {
@@ -6,9 +8,12 @@ namespace WordCounter
     {
         static void Main(string[] args)
         {
-              WordCounterVowels text = new WordCounterVowels("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500");
+            string text = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500";
 
-            Console.WriteLine(text.CountWords());
+            Console.WriteLine($"Unique words: {new WordCounter(text).CountWords()}");
+            Console.WriteLine($"Words that begin with vowels: {new WordCounterVowels(text).CountWords()}");
+            Console.WriteLine($"Words with more than two characters: {new WordCounterTwoChar(text).CountWords()}");
+            Console.WriteLine($"Words that begin with capital: {new WordCounterCapitalCase(text).CountWords()}");
 
         }
     }
