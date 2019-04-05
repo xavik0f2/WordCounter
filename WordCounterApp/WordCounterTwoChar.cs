61d2337724725fc018a6350669ee8d10def3eadd
Xavier Llanga <xavier.llanga.perez@avanade.com>
2019-04-05 21:58:21 +0200

    WordCounterTwoChar implemented and tested

--- WordCounterApp/WordCounterTwoChar.cs
+++ WordCounterApp/WordCounterTwoChar.cs
@@ -1,10 +1,18 @@
+using System.Collections.Generic;
+
 namespace WordCounter
 {
     public class WordCounterTwoChar : IWordCounter
     {
+        private string Text;
+        public WordCounterTwoChar(string text)
+        {
+            this.Text = text;
+        }
         public int CountWords()
         {
-            throw new System.NotImplementedException();
+            return new WordSplitter(Text).SplittedWords
+            .FindAll(word => word.Length > 2).Count;
         }
     }
 }
