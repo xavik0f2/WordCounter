58aff511a2d6579434af69f2cebac351f8bfab6d
Xavier Llanga <xavier.llanga.perez@avanade.com>
2019-04-05 23:09:51 +0200

    Implemented WordCounterCapitalCase logic

--- WordCounterApp/WordCounterCapitalCase.cs
+++ WordCounterApp/WordCounterCapitalCase.cs
@@ -1,10 +1,25 @@
+using System;
 namespace WordCounter
 {
     public class WordCounterCapitalCase : IWordCounter
     {
+        private string Text { get; set; }
+
+        public WordCounterCapitalCase(string text)
+        {
+            Text = text;
+        }
         public int CountWords()
         {
-            throw new System.NotImplementedException();
+            return new WordSplitter(Text).SplittedWords.FindAll(word => StartsWithCapitalCase(word)).Count;
+        }
+
+        public Boolean StartsWithCapitalCase(string word)
+        {
+            if (word.Length == 0)
+                return false;
+
+            return char.IsUpper(word, 0);
         }
     }
 }
